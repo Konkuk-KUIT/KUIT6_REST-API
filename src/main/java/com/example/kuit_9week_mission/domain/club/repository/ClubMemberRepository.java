e7bd95ce752a0ff713d8ed91043a4584e0a66072
ksg <ksg001227@naver.com>
2025-10-23 23:44:15 +0900

    Rebase

--- src/main/java/com/example/kuit_9week_mission/domain/club/repository/ClubMemberRepository.java
+++ src/main/java/com/example/kuit_9week_mission/domain/club/repository/ClubMemberRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class ClubMemberRepository {
-
     private final JdbcTemplate jdbc;
 
     // TODO: TODO 6을 구현하기 위해선 Club_Members 와 Clubs 테이블간의 JOIN 을 적절히 활용해야한다!
